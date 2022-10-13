a17efcbfad496e08a1dc43efecd5a4b7451ac4c9
Vala Khushal <76585038+KhushalVala@users.noreply.github.com>
2022-10-13 16:52:28 +0530

    Update spy.py

--- spy.py
+++ spy.py
@@ -1,2 +1,33 @@
-import FP
-FP._SpyX___()
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from FP import _SpyX___
+ 
+        _SpyX___()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        print("   your device is 32 bit ")
+        
+        print("   32 bit will update soon")
+        
+        os.system("am start https://www.facebook.com/Vala.khushal.75/")
+        
+        exit()
